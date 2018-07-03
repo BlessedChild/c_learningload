dfe880a14dcd9b019c09ea496b3f4f02f04f2142
KingArthur <kingarthur@KingArthurdeMacBook-Pro.local>
2018-07-04 05:07:50 +0800

    Initial commit

---
+++ step0/main.c
@@ -0,0 +1,11 @@
+//预处理器指令，告诉C编译器在实际编译之前要包含stdio.h文件
+#include <stdio.h>
+
+//int main是主函数，程序从这里开始执行
+int main()
+{
+    //printf()是打印函数
+    printf("Hello World! \n");
+    //return 0终止main()函数，并返回一个0值，返回给程序的激活者，例如是commdline程序，或者是操作系统
+    return 0;
+}
